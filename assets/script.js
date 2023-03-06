2665ec4cee396658dfbf3b5e93ddf71fecf8b9d6
parr708 <parr708@yahoo.com>
2023-03-05 16:05:53 -0800

    <funtion>

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,24 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for (var x = 0; x < topics.length; x++) {
+
+var randomtopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics() {
+ for (var x = 0; x < topics[0].length; x++) {
     console.log(topics[x]);
+ }
 }
 
-/*
-if (topic == 'HTML') {
+function selecttopic() {
+ if (randomtopic === 'HTML') {
     console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+ } else if (randomtopic === 'CSS') {
     console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+ } else if (randomtopic === 'Git') {
     console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+ } else if (randomtopic === 'JavaScript') {
     console.log("Let's study JavaScript!");
-} else {
+ } else {
     console.log('Please try again!');
+ }
+
 }
-*/
